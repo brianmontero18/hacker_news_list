5a83cc25b6a47141fe5bce67055feb37b66ce1c3
Brian Montero <brian.montero@unioncrate.com>
2021-10-19 22:44:49 -0300

    updating link_preview_key

--- src/api/useStories.js
+++ src/api/useStories.js
@@ -1,12 +1,12 @@
 import { useQuery, useQueries } from 'react-query';
 
-const LINK_PREVIEW_KEY = 'b1b9077d02a33accdd1e11f8dec9902a';
+const LINK_PREVIEW_KEY = 'e514d3559aa28b2214c37b6c28f27559';
 const HACKER_NEWS_STORY_LIST_KEY = 'hacker_news_top_story_list_key';
 const HACKER_NEWS_STORY_KEY = 'hacker_news_story_key';
 const HACKER_NEWS_URL = 'https://hacker-news.firebaseio.com/v0';
 const SHOW_STORIES_URL = `${HACKER_NEWS_URL}/beststories.json`;
 const SHOW_ITEM_URL = `${HACKER_NEWS_URL}/item`;
-const LINK_PREVIEW_URL = `http://api.linkpreview.net/?key=${LINK_PREVIEW_KEY}`;
+const LINK_PREVIEW_URL = 'https://api.linkpreview.net';
 const PAGE_SIZE = 8;
 const emptyObject = {};
 
@@ -30,14 +30,21 @@ function getStoriesList(res) {
 
 function handleStory(story) {
 	return {
+		placeholderData: emptyObject,
 		queryKey: [HACKER_NEWS_STORY_KEY, story],
 		queryFn: async () => {
 			const storyResp = await fetch(`${SHOW_ITEM_URL}/${story}.json`);
 			const storyBody = await storyResp.json();
 
-			const previewResp = await fetch(
-				`${LINK_PREVIEW_URL}&q=${storyBody.url}`
-			);
+			const previewResp = await fetch(LINK_PREVIEW_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: `key=${LINK_PREVIEW_KEY}&q=${encodeURIComponent(
+					storyBody.url
+				)}`,
+			});
 			const previewBody = await previewResp.json();
 
 			return {
@@ -45,6 +52,5 @@ function handleStory(story) {
 				preview: previewBody,
 			};
 		},
-		placeholderData: emptyObject,
 	};
 }
